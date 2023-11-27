55c4fba16c5846bd2c45651c694a2195afae802f
ChHasnainNaveed <151489438+ChHasnainNaveed@users.noreply.github.com>
2023-11-27 21:43:41 +0500

    Create Assignment 1.java

---
+++ .github/workflows/Assignment 1.java
@@ -0,0 +1,25 @@
+class Color{
+String Color;
+public void printColor() {
+System.out.println("The color of this pen is + this.color);
+}
+}
+
+public class OOP {
+
+public static void main(String args[]) {
+
+Pen p1 = new Pen();
+
+pl.color="blue";
+Pen p2 = new Pen(); 
+p2.color black";
+
+Pen p3 = new Pen();
+
+p3.color = "red"; 
+pl.printColor();
+p2.printColor();
+p3.printColor();
+}
+}
