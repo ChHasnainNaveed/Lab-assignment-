91f4b92a83275a730e6904ede216e04aa147ac4b
ChHasnainNaveed <151489438+ChHasnainNaveed@users.noreply.github.com>
2023-11-27 21:35:00 +0500

    Create Assignment 2.java

---
+++ .github/workflows/Assignment 2.java
@@ -0,0 +1,29 @@
+class Student {
+
+String name;
+
+int age;
+
+public void getInfo() {
+
+System.out.println("The name of this Student is " + this.name);
+
+System.out.println("The age of this Student is " + this.age);
+}                                                                  }
+public class OOP (
+
+public static void main(String args[]) {
+
+Student s1 = new Student();
+
+s1.name = "Hasnain";
+
+s1.age = 20;
+
+s1.getInfo();
+
+Student s * 2 = new Student(); s2.name = "Naveed";
+
+s2.age = 22;
+
+s2.getInfo();
